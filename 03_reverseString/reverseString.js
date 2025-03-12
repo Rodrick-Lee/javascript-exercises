0c2858fcc37b6390b2771559056bbc86a5db29c1
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-12 16:24:23 -0600

    solution exercises 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseWord = []
+    let splitWord = string.split("");
+    let j = 0;
+    for (let i = splitWord.length-1; i >= 0; i--) {
+        reverseWord[j] = splitWord[i]; 
+        j++;
+    }
+    return reverseWord.join("");
+    
 };
 
 // Do not edit below this line
