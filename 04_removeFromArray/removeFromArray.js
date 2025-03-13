0a8c6c6cb08818dadf5b34efd50dcc030de14fd9
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-13 10:18:58 -0600

    solution exercises 04_removeFronArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theWords) {
+    let newArray = array.filter(val => !theWords.includes(val));
+    console.log(newArray)
+    return newArray 
 };
 
 // Do not edit below this line
