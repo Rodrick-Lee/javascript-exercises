6261a211eb4402684c6fe2cd262d0a8bb816871f
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-13 11:16:51 -0600

    solution exercises 05_sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+    if (num1 >= 0 && Number.isInteger(num1) && num2 >= 0 &&  Number.isInteger(num2)) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum
+    }
+    return "ERROR"
+        
+      
 };
 
+sumAll(1.1, 2)
 // Do not edit below this line
 module.exports = sumAll;
