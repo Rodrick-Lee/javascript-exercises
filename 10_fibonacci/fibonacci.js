d1f14f39abdc7ac8e6bd2ce7659c29a14b63a55d
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-19 12:59:33 -0600

    solution exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+let currentNum = 1; 
+let previousNum = 0; 
 
+if(index == 0) return previousNum
+if(index < 0) return "OOPS"
+
+for (let i = 1; i < index; i++) {
+    let nextNum = currentNum + previousNum
+    previousNum = currentNum
+    currentNum = nextNum
+}
+return currentNum
 };
 
 // Do not edit below this line
