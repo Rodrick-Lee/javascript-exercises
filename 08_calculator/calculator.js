46b50f4269b5c7c4582da312af9fdb0536f5707b
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-19 11:55:00 -0600

    solution exercise 08_calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, num) => sum+num,0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, num) => total * num,1)
+  
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	while(num > 0){
+    total *= num
+    num--
+  }
+  return total
 };
 
 // Do not edit below this line
