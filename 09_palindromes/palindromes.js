b3fcb84b05b1f42b4e21471e2dec8e37169c9f3d
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-19 12:42:46 -0600

    solution exercise 09_palindrones

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const ALPHANUMERIC = "abcdefghijklmnopqrstuvxyz0123456789"
+    const strClear = str
+        .toLowerCase()
+        .split("")
+        .filter((character) => ALPHANUMERIC.includes(character))
+        .join("")
 
+    const strReverse = strClear
+        .split("")
+        .reverse()
+        .join("")
+
+    return (strClear == strReverse) ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
